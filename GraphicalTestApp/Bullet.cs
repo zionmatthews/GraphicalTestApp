6a14eaeeca53ae65b865552113b6d93ab789eb20
zion matthews <zman5682@gmail.com>
2019-12-11 08:43:48 -0600

    Working on bullet

--- GraphicalTestApp/Bullet.cs
+++ GraphicalTestApp/Bullet.cs
@@ -6,13 +6,22 @@ using System.Threading.Tasks;
 
 namespace GraphicalTestApp
 {
-    class Bullet
+    class Bullet : Entity
     {
         private Sprite _bullet;
+        private AABB _box;
 
-        public Bullet(float x, float y)
+        public Bullet(float x, float y) : base(x, y)
         {
+            _bullet = new Sprite("Bullet-Transparent-Background.png");
+            _box = new AABB(25, 25);
 
+
+
+            AddChild(_bullet);
+            AddChild(_box);
         }
+
+
     }
 }
--- GraphicalTestApp/Turret.cs
+++ GraphicalTestApp/Turret.cs
@@ -10,9 +10,11 @@ namespace GraphicalTestApp
     {
         private Sprite _turret;
 
+
         public Turret(float x, float y)
         {
             _turret = new Sprite("barrelBlue.png");
+
 
             _turret.X = -8.0f;
             _turret.Y = -45;
@@ -23,8 +25,10 @@ namespace GraphicalTestApp
             AddChild(_turret);
 
 
+
             OnUpdate += Rotateleft;
             OnUpdate += Rotateright;
+            OnUpdate += Fire;
         }
 
         public void Rotateleft(float deltatime)
@@ -48,10 +52,29 @@ namespace GraphicalTestApp
             }
         }
 
-        public void Fire()
+        public void Fire(float deltatime)
         {
-
+            //Shoots the turret (Space Bar)
+            if (Input.IsKeyDown(32))
+            {
+                Bullet _bullet = new Bullet(XAbsolute, YAbsolute);
+                Vector3 direction = GetDirectionAbsolute();
+                _bullet.Rotate(GetRotationAbsolute());
+                _bullet.X += direction.x * -50f;
+                _bullet.Y += direction.y * -50f;
+                _bullet.XVelocity += direction.x * -300f;
+                _bullet.YVelocity += direction.y * -300f;
+                Parent.AddChild(_bullet);
+            }
         }
+
+        /* Bullet _bullet = new Bullet(XAbsolute, YAbsolute);
+         Vector3 direction = GetDirectionAbsolute();
+         direction.Normalize();
+                 direction *= 50;
+                 _bullet.XVelocity = direction.x;
+                 _bullet.YVelocity = direction.y;
+                 Parent.Parent.AddChild(_bullet); */
 
 
     }
